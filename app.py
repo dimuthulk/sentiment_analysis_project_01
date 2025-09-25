00ef83db7b05d5633c2032cfe756c12a96f05dc6
Dimuthu Lakmal <info.dlrathnayaka@gmail.com>
2025-09-25 17:11:30 +0530

    build web app

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask,render_template,request,redirect
+from helper import preprocessing, vectorizer, get_prediction, tokens
+
+
+app = Flask(__name__)
+
+data = dict()
+reviews = []
+positive = 0
+negative = 0
+
+@app.route("/")
+def index():
+    data['reviews'] = reviews
+    data['positive'] = positive
+    data['negative'] = negative
+    return render_template("index.html", data=data)
+
+@app.route("/", methods=["POST"])
+def my_post():
+    text = request.form['text']
+    preprocessed_text = preprocessing(text)
+    vectorized_text = vectorizer(preprocessed_text, vocabulary=tokens)
+    prediction = get_prediction(vectorized_text)
+    
+    if prediction == 'positive':
+        global positive
+        positive += 1
+    else:
+        global negative
+        negative += 1
+    reviews.insert(0, text)
+    return redirect(request.url)
+
+
+if __name__ == "__main__":
+    app.run()
